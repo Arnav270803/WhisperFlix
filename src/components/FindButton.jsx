4837e65a604c1e23cfd80b02e2e633654a134259
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-08-30 15:08:19 +0530

    Adding login button:

--- src/components/FindButton.jsx
+++ src/components/FindButton.jsx
@@ -1,15 +1,16 @@
-// FindButton.jsx
 import React from 'react'
 
-const FindButton = ({ onGenraClick }) => { // Add onGenraClick prop
+const FindButton = ({ handleFavouriteClick}) => { 
   return (
-    <div className='flex justify-center mt-8'>
-      <button
-        onClick={onGenraClick} // Add onClick handler
-        className='px-4 py-1.5 bg-transparent border border-orange-400 text-orange-400 font-medium text-sm rounded hover:bg-orange-400 hover:text-black transition-all duration-300 tracking-wide'>
-        Find
-      </button>
-    </div>
+    <div className='flex mt-4 justify-center'>
+          <button 
+          onClick={handleFavouriteClick} // Add onClick handler
+          className='rounded-full  border border-white/30 transition-all duration-300 px-6 py-2 cursor-pointer hover:bg-white/10 hover:border-white/60 hover:scale-105 backdrop-blur-sm bg-black/20'>
+          <span className='bg-gradient-to-r from-orange-500 via-orange-400 to-yellow-300 bg-clip-text text-transparent font-medium'>
+            Favourite
+          </span>
+        </button>
+        </div>
   )
 }
 
